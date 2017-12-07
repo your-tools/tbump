75804758174be0d4313cd8b4012b9706126f7146
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2017-12-07 17:53:33 +0100

    setup.py: add missing deps

--- setup.py
+++ setup.py
@@ -19,6 +19,9 @@ setup(name="tbump",
       packages=find_packages(),
       include_package_data=True,
       install_requires=[
+        "attrs",
+        "path.py",
+        "python-cli-ui",
         "toml",
       ],
       classifiers=[
