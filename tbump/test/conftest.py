247f34a6d88c6d8430770d5dc7b502e65fb72fe6
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2017-12-13 13:46:18 +0100

    test_main: introduce a test_repo fixture

--- tbump/test/conftest.py
+++ tbump/test/conftest.py
@@ -3,6 +3,8 @@ import re
 import path
 import pytest
 
+import tbump.git
+
 
 @pytest.fixture()
 def tmp_path(tmpdir):
@@ -13,3 +15,37 @@ def tmp_path(tmpdir):
 def test_path():
     this_dir = path.Path(__file__).parent
     return this_dir.abspath()
+
+
+
+def setup_repo(tmp_path, test_path):
+    src_path = tmp_path.joinpath("src")
+    src_path.mkdir()
+    test_path.joinpath("tbump.toml").copy(src_path)
+    test_path.joinpath("VERSION").copy(src_path)
+    test_path.joinpath("package.json").copy(src_path)
+    test_path.joinpath("pub.js").copy(src_path)
+    tbump.git.run_git(src_path, "init")
+    tbump.git.run_git(src_path, "add", ".")
+    tbump.git.run_git(src_path, "commit", "--message", "initial commit")
+    return src_path
+
+
+def setup_remote(tmp_path):
+    git_path = tmp_path.joinpath("git")
+    git_path.mkdir()
+    remote_path = git_path.joinpath("repo.git")
+    remote_path.mkdir()
+    tbump.git.run_git(remote_path, "init", "--bare")
+
+    src_path = tmp_path.joinpath("src")
+    tbump.git.run_git(src_path, "remote", "add", "origin", remote_path)
+    tbump.git.run_git(src_path, "push", "-u", "origin", "master")
+    return src_path
+
+
+@pytest.fixture
+def test_repo(tmp_path, test_path):
+    res = setup_repo(tmp_path, test_path)
+    setup_remote(tmp_path)
+    return res
--- tbump/test/test_main.py
+++ tbump/test/test_main.py
@@ -18,36 +18,10 @@ def assert_in_file(file_name, expected_line):
     assert False, "No line found matching %s" % expected_line
 
 
-def setup_test(test_path, tmp_path):
-    src_path = tmp_path.joinpath("src")
-    src_path.mkdir()
-    test_path.joinpath("tbump.toml").copy(src_path)
-    test_path.joinpath("VERSION").copy(src_path)
-    test_path.joinpath("package.json").copy(src_path)
-    tbump.git.run_git(src_path, "init")
-    tbump.git.run_git(src_path, "add", ".")
-    tbump.git.run_git(src_path, "commit", "--message", "initial commit")
-    setup_remote(tmp_path)
-    return src_path
+def test_replaces(test_repo):
+    tbump.main.main(["-C", test_repo, "1.2.42-alpha-1", "--non-interactive"])
 
-
-def setup_remote(tmp_path):
-    git_path = tmp_path.joinpath("git")
-    git_path.mkdir()
-    remote_path = git_path.joinpath("repo.git")
-    remote_path.mkdir()
-    tbump.git.run_git(remote_path, "init", "--bare")
-
-    src_path = tmp_path.joinpath("src")
-    tbump.git.run_git(src_path, "remote", "add", "origin", remote_path)
-    tbump.git.run_git(src_path, "push", "-u", "origin", "master")
-
-
-def test_replaces(tmp_path, test_path):
-    src_path = setup_test(test_path, tmp_path)
-    tbump.main.main(["-C", src_path, "1.2.42-alpha-1", "--non-interactive"])
-
-    toml_path = src_path.joinpath("tbump.toml")
+    toml_path = test_repo.joinpath("tbump.toml")
     new_toml = toml.loads(toml_path.text())
     assert new_toml["version"]["current"] == "1.2.42-alpha-1"
 
@@ -55,69 +29,63 @@ def test_replaces(tmp_path, test_path):
     assert_in_file("package.json", '"other-dep": "1.2.41"')
 
 
-def test_commit_and_tag(tmp_path, test_path):
-    src_path = setup_test(test_path, tmp_path)
-    tbump.main.main(["-C", src_path, "1.2.42-alpha-1", "--non-interactive"])
+def test_commit_and_tag(test_repo):
+    tbump.main.main(["-C", test_repo, "1.2.42-alpha-1", "--non-interactive"])
 
-    rc, out = tbump.git.run_git(src_path, "log", "--oneline", raises=False)
+    rc, out = tbump.git.run_git(test_repo, "log", "--oneline", raises=False)
     assert rc == 0
     assert "Bump to 1.2.42-alpha-1" in out
 
-    rc, out = tbump.git.run_git(src_path, "tag", "--list", raises=False)
+    rc, out = tbump.git.run_git(test_repo, "tag", "--list", raises=False)
     assert rc == 0
     assert out == "v1.2.42-alpha-1"
 
 
-def test_abort_if_dirty(tmp_path, test_path, message_recorder):
-    src_path = setup_test(test_path, tmp_path)
-    src_path.joinpath("VERSION").write_text("unstaged changes\n", append=True)
+def test_abort_if_dirty(test_repo, message_recorder):
+    test_repo.joinpath("VERSION").write_text("unstaged changes\n", append=True)
 
     with pytest.raises(SystemExit) as e:
-        tbump.main.main(["-C", src_path, "1.2.42-alpha-1", "--non-interactive"])
+        tbump.main.main(["-C", test_repo, "1.2.42-alpha-1", "--non-interactive"])
     assert message_recorder.find("dirty")
 
 
-def test_abort_if_tag_exists(tmp_path, test_path, message_recorder):
-    src_path = setup_test(test_path, tmp_path)
-    tbump.git.run_git(src_path, "tag", "v1.2.42")
+def test_abort_if_tag_exists(test_repo, message_recorder):
+    tbump.git.run_git(test_repo, "tag", "v1.2.42")
 
     with pytest.raises(SystemExit) as e:
-        tbump.main.main(["-C", src_path, "1.2.42", "--non-interactive"])
+        tbump.main.main(["-C", test_repo, "1.2.42", "--non-interactive"])
     assert message_recorder.find("1.2.42 already exists")
 
 
-def test_abort_if_file_does_not_change(tmp_path, test_path, message_recorder):
-    src_path = setup_test(test_path, tmp_path)
-    invalid_src = src_path.joinpath("foo.txt")
+def test_abort_if_file_does_not_change(test_repo, message_recorder):
+    invalid_src = test_repo.joinpath("foo.txt")
     invalid_src.write_text("this is foo")
-    tbump_path = src_path.joinpath("tbump.toml")
+    tbump_path = test_repo.joinpath("tbump.toml")
     tbump_path.write_text("""\
     [[file]]
     src = "foo.txt"
     """, append=True)
-    tbump.git.run_git(src_path, "add", ".")
-    tbump.git.run_git(src_path, "commit", "--message", "add foo.txt")
+    tbump.git.run_git(test_repo, "add", ".")
+    tbump.git.run_git(test_repo, "commit", "--message", "add foo.txt")
 
     with pytest.raises(SystemExit):
-        tbump.main.main(["-C", src_path, "1.2.42", "--non-interactive"])
+        tbump.main.main(["-C", test_repo, "1.2.42", "--non-interactive"])
     assert "foo.txt did not change"
 
 
-def test_interactive_push(tmp_path, test_path, message_recorder, mock):
-    src_path = setup_test(test_path, tmp_path)
+def test_interactive_push(test_repo, message_recorder, mock):
     ask_mock = mock.patch("ui.ask_yes_no")
     ask_mock.return_value = True
-    tbump.main.main(["-C", src_path, "1.2.42"])
+    tbump.main.main(["-C", test_repo, "1.2.42"])
     ask_mock.assert_called_with("OK to push", default=False)
-    rc, out = tbump.git.run_git(src_path, "ls-remote", raises=False)
+    rc, out = tbump.git.run_git(test_repo, "ls-remote", raises=False)
     assert rc == 0
     assert "tags/v1.2.42" in out
 
 
-def test_do_not_add_untracked_files(tmp_path, test_path):
-    src_path = setup_test(test_path, tmp_path)
-    src_path.joinpath("untracked.txt").write_text("please don't add me")
-    tbump.main.main(["-C", src_path, "1.2.42", "--non-interactive"])
-    rc, out = tbump.git.run_git(src_path, "show", "--stat", "HEAD", raises=False)
+def test_do_not_add_untracked_files(test_repo):
+    test_repo.joinpath("untracked.txt").write_text("please don't add me")
+    tbump.main.main(["-C", test_repo, "1.2.42", "--non-interactive"])
+    rc, out = tbump.git.run_git(test_repo, "show", "--stat", "HEAD", raises=False)
     assert rc == 0
     assert "untracked.txt" not in out
