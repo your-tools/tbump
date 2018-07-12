7c58af330e46722d252566ce51923b89cc2a7e57
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2018-07-12 11:21:13 +0200

    Bump to 3.0.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def get_long_description():
 
 
 setup(name="tbump",
-      version="3.0.0",
+      version="3.0.1",
       description="Bump software releases",
       long_description=get_long_description(),
       url="https://github.com/SuperTanker/tbump",
