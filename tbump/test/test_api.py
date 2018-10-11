64ad21081531ed2ba8fade62a571455b43366806
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2018-10-11 16:14:37 +0200

    Fix lint warnings

--- tbump/test/test_api.py
+++ tbump/test/test_api.py
@@ -6,12 +6,12 @@ from tbump.test.conftest import file_contains
 
 def test_bump_files_defaults_to_working_dir(test_repo: Path) -> None:
     with test_repo:
-        bumper = tbump.bump_files("1.2.42")
+        tbump.bump_files("1.2.42")
 
     assert file_contains(test_repo / "package.json", '"version": "1.2.42"')
 
 
 def test_bump_files_with_repo_path(test_repo: Path) -> None:
-    bumper = tbump.bump_files("1.2.42", test_repo)
+    tbump.bump_files("1.2.42", test_repo)
 
     assert file_contains(test_repo / "package.json", '"version": "1.2.42"')
