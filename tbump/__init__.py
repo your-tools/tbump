e435d4eecafb848959d1824a4c87baf23144170d
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2018-10-12 11:13:18 +0200

    Add a meaningful __str__ for tbump's errors

--- tbump/__init__.py
+++ tbump/__init__.py
@@ -1,5 +1,13 @@
+import pprint
+
+
 class Error(Exception):
     def print_error(self) -> None:
         pass
+
+    def __str__(self) -> str:
+        pp = pprint.PrettyPrinter(indent=4)
+        return pp.pformat(vars(self))
+
 
 from .file_bumper import bump_files  # noqa
