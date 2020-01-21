94b8a23c031b0c1ac1ff03b4b89a644aead936c1
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2020-01-21 11:27:04 +0100

    Fix overzealous formatting by black

--- tbump/file_bumper.py
+++ tbump/file_bumper.py
@@ -33,38 +33,22 @@ class Patch(tbump.action.Action):
         self.new_line = new_line
 
     def print_self(self) -> None:
+        # fmt: off
         ui.info(
-            ui.red,
-            "- ",
-            ui.reset,
-            ui.bold,
-            self.src,
-            ":",
-            ui.reset,
-            ui.darkgray,
-            self.lineno + 1,
-            ui.reset,
-            " ",
-            ui.red,
-            self.old_line.strip(),
+            ui.red, "- ", ui.reset,
+            ui.bold, self.src, ":", ui.reset,
+            ui.darkgray, self.lineno + 1, ui.reset,
+            " ", ui.red, self.old_line.strip(),
             sep="",
         )
         ui.info(
-            ui.green,
-            "+ ",
-            ui.reset,
-            ui.bold,
-            self.src,
-            ":",
-            ui.reset,
-            ui.darkgray,
-            self.lineno + 1,
-            ui.reset,
-            " ",
-            ui.green,
-            self.new_line.strip(),
+            ui.green, "+ ", ui.reset,
+            ui.bold, self.src, ":", ui.reset,
+            ui.darkgray, self.lineno + 1, ui.reset,
+            " ", ui.green, self.new_line.strip(),
             sep="",
         )
+        # fmt: on
 
     def do(self) -> None:
         self.apply()
