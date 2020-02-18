9b726cd87fd948b10316c0838e24fd7655575546
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2020-02-18 15:05:51 +0100

    Rewrite test hooks
    It was not a good idea to have complex logic inside them.
    Just make them generate stamp files is enough.
    
    Also, check that hook have access to both current_version and
    new_version

--- tbump/test/data/before.py
+++ tbump/test/data/before.py
@@ -1,13 +1,15 @@
 """ Fake hook used for tetsting.
-Just write a file named after-hook.stamp when called,
+Just write a file named before-hook.stamp when called,
 so that test code can check if the hook ran
 """
 
+import sys
 from path import Path
 
 
 def main() -> None:
-    Path("after-hook.stamp").write_text("")
+    current, new = sys.argv[1:]
+    Path("before-hook.stamp").write_text(current + " -> " + new)
 
 
 if __name__ == "__main__":
