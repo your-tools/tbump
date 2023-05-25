3bb15222c0c926ae1aa15e0e1ff23daa327d7b8c
Dimitri Merejkowsky <dimitri@dmerej.info>
2023-05-25 17:22:23 +0200

    Expose a bug in `--no-atomic` handling ?

--- tbump/test/test_cli.py
+++ tbump/test/test_cli.py
@@ -15,7 +15,7 @@ from tbump.file_bumper import (
 )
 from tbump.git import run_git, run_git_captured
 from tbump.git_bumper import DirtyRepository, NoTrackedBranch, RefAlreadyExists
-from tbump.test.conftest import file_contains
+from tbump.test.conftest import GitRecorder, file_contains
 
 
 def files_bumped(test_repo: Path, config_path: Optional[Path] = None) -> bool:
@@ -137,7 +137,9 @@ def test_end_to_end_using_tbump_toml(test_repo: Path) -> None:
     assert bump_done(test_repo, previous_commit)
 
 
-def test_end_to_end_using_tbump_toml_no_atomic(test_repo: Path) -> None:
+def test_end_to_end_using_tbump_toml_no_atomic(
+    test_repo: Path, git_recorder: GitRecorder
+) -> None:
     _, previous_commit = run_git_captured(test_repo, "rev-parse", "HEAD")
     run_tbump(
         [
@@ -150,6 +152,8 @@ def test_end_to_end_using_tbump_toml_no_atomic(test_repo: Path) -> None:
     )
 
     assert bump_done(test_repo, previous_commit)
+    last_command = git_recorder.commands()[-1]
+    assert "--atomic" not in last_command
 
 
 def test_end_to_end_using_pyproject_toml(test_pyproject_repo: Path) -> None:
